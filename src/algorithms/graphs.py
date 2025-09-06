48b7acf3836444da358d85defb4949c92acc34f7
Alexander Maxwell <74562268+Alexmaxdout@users.noreply.github.com>
2025-09-06 16:52:49 -0400

    Update graphs.py

--- src/algorithms/graphs.py
+++ src/algorithms/graphs.py
@@ -1,1 +1,21 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = []
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.append(node)
+            queue.extend([n for n in graph[node] if n not in visited])
+    return visited
+
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = []
+    visited.append(start)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+    return visited
 
