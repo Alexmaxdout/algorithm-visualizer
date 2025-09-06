d921cbbdb181f49ed4a5d53a15b70decfe6c016c
Alexander Maxwell <74562268+Alexmaxdout@users.noreply.github.com>
2025-09-06 16:52:29 -0400

    Update searching.py

--- src/algorithms/searching.py
+++ src/algorithms/searching.py
@@ -1,1 +1,18 @@
+def linear_search(arr, target):
+    for i, val in enumerate(arr):
+        if val == target:
+            return i
+    return -1
+
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
 
